24199c026868956390e5846e9d4bc72941e25129
STUDIO-943 <100957640+STUDIO-943@users.noreply.github.com>
2022-10-17 22:44:36 +0900

    char anim frame setting
    char anim frame setting (pre-setting)

--- Char_move.py
+++ Char_move.py
@@ -29,3 +29,48 @@ def handle_events():
                 dir_y -= 1
             elif event.key == SDLK_s:
                 dir_y += 1 
+
+def draw (bottom): 
+
+    global x, y
+    global dir_x, dir_y
+    global anim_frame
+
+    clear_canvas()
+    background.draw(,)
+    char.clip_draw(frame * 100, bottom, 100, 100, x, y)
+    update_canvas()
+
+    handle_events()
+
+    frame = (frame + 1) % 8
+
+    x += dir_x * 5
+    y += dir_y * 5
+
+    delay(0.05)
+
+open_canvas()
+background = load_image('')
+char = load_image('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
