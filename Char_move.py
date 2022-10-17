d6d750ee6709169ba95ce7f831ac44ebe59ced72
STUDIO-943 <100957640+STUDIO-943@users.noreply.github.com>
2022-10-17 22:32:09 +0900

    Key setting
    Key setting for basic movements

--- Char_move.py
+++ Char_move.py
@@ -1,1 +1,31 @@
 from pico2d import *
+
+def handle_events():
+    global running # dir은 다이랙션 
+    global dir_x
+    global dir_y
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_d:
+                dir_x += 1
+            elif event.key == SDLK_a:
+                dir_x -= 1
+            elif event.key == SDLK_w:
+                dir_y += 1
+            elif event.key == SDLK_s:
+                dir_y -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_d:
+                dir_x -= 1
+            elif event.key == SDLK_a:
+                dir_x += 1
+            elif event.key == SDLK_w:
+                dir_y -= 1
+            elif event.key == SDLK_s:
+                dir_y += 1 
